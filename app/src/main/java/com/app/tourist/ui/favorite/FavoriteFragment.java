b6d784d1acc33ad7b8e97ff0f3fb5fb93108bc27
Robsona Tsiory <robsonatsiory07@gmail.com>
2023-08-02 17:46:07 +0300

    [add] navigation project

--- app/src/main/java/com/app/tourist/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/app/tourist/ui/favorite/FavoriteFragment.java
@@ -11,12 +11,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.app.tourist.R;
+import com.app.tourist.databinding.FragmentFavoriteBinding;
+import com.app.tourist.databinding.FragmentProfileBinding;
+import com.app.tourist.ui.profile.ProfileViewModel;
 
 public class FavoriteFragment extends Fragment {
 
     private FavoriteViewModel mViewModel;
+    private FragmentFavoriteBinding binding;
 
     public static FavoriteFragment newInstance() {
         return new FavoriteFragment();
@@ -25,7 +30,15 @@ public class FavoriteFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        FavoriteViewModel favoriteViewModel =
+                new ViewModelProvider(this).get(FavoriteViewModel.class);
+
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textFavoris;
+        favoriteViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
     }
 
     @Override
--- app/src/main/java/com/app/tourist/ui/favorite/FavoriteViewModel.java
+++ app/src/main/java/com/app/tourist/ui/favorite/FavoriteViewModel.java
@@ -1,7 +1,19 @@
 package com.app.tourist.ui.favorite;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class FavoriteViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    private final MutableLiveData<String> mText;
+
+    public FavoriteViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Favoris fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
