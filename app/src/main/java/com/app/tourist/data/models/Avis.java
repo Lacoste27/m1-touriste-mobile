2f5f3a5db3acc582da6d7479ef1ef82cdeefc84d
890Tom <zafi.tommy@gmail.com>
2023-08-05 18:50:27 +0300

    [SITES] Dynamic List

---
+++ app/src/main/java/com/app/tourist/data/models/Avis.java
@@ -0,0 +1,22 @@
+package com.app.tourist.data.models;
+
+public class Avis {
+    Integer note;
+    String commentaire;
+
+    public Integer getNote() {
+        return note;
+    }
+
+    public void setNote(Integer note) {
+        this.note = note;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+}
--- app/src/main/java/com/app/tourist/ui/view/home/HomeFragment.java
+++ app/src/main/java/com/app/tourist/ui/view/home/HomeFragment.java
@@ -3,9 +3,12 @@ package com.app.tourist.ui.view.home;
 import com.app.tourist.R;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,21 +19,45 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.tourist.data.models.ItemsModel;
+import com.app.tourist.data.models.SitesModel;
 import com.app.tourist.databinding.FragmentHomeBinding;
+import com.app.tourist.domain.entities.Site;
 import com.app.tourist.ui.adapter.ItemsAdapter;
+import com.app.tourist.ui.adapter.ListItemAdapter;
+import com.app.tourist.ui.adapter.SitesAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import com.app.tourist.data.service.SiteService;
 
 public class HomeFragment extends Fragment {
 
+    OkHttpClient client;
+    String getUrl = "https://m1-tourist-test.onrender.com/api/sites";
     private FragmentHomeBinding binding;
-    private RecyclerView.Adapter adapterPopular, adapterNew;
-    private RecyclerView recyclerViewPopular, recyclerViewNew;
-    private View view;
+    private BaseAdapter adapterList;
+    private ListView listViewAll;
+
+    private SiteService siteService;
+
+    //private View view;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        siteService = new SiteService();
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         View views = inflater.inflate(R.layout.fragment_home, container, false);
@@ -38,56 +65,60 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        this.view = views;
+        //this.view = views;
+
+        ///API FETCH
+        client = new OkHttpClient.Builder().callTimeout(30, TimeUnit.SECONDS).build();
+
+        get(root);
+
         return root;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        ArrayList<ItemsModel> itemsArrayList = new ArrayList<>();
-
-        itemsArrayList.add(new ItemsModel("House with a great view", "San Francisco, CA 94110"," This is 2 bed /1 bath home boasts an enormous, \n", 2,1,840353, "pic1", true));
-        itemsArrayList.add(new ItemsModel("House with a great view", "San Francisco, CA 94110"," This is 2 bed /1 bath home boasts an enormous, \n", 2,1,840353, "pic1", true));
-        itemsArrayList.add(new ItemsModel("House with a great view", "San Francisco, CA 94110"," This is 2 bed /1 bath home boasts an enormous, \n", 2,1,840353, "pic1", true));
-
-        recyclerViewPopular = (RecyclerView) view.findViewById(R.id.viewPopular);
-        recyclerViewNew = (RecyclerView) view.findViewById(R.id.viewNews);
-
-        recyclerViewPopular.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
-        recyclerViewNew.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
-
-        adapterNew = new ItemsAdapter(itemsArrayList);
-        adapterPopular = new ItemsAdapter(itemsArrayList);
-
-        recyclerViewPopular.setAdapter(adapterPopular);
-        recyclerViewNew.setAdapter(adapterNew);
-    }
-
-    public void setRecyclerView(){
-        ArrayList<ItemsModel> itemsArrayList = new ArrayList<>();
-
-        itemsArrayList.add(new ItemsModel("House with a great view", "San Francisco, CA 94110"," This is 2 bed /1 bath home boasts an enormous, \n", 2,1,840353, "pic1", true));
-        itemsArrayList.add(new ItemsModel("House with a great view", "San Francisco, CA 94110"," This is 2 bed /1 bath home boasts an enormous, \n", 2,1,840353, "pic1", true));
-        itemsArrayList.add(new ItemsModel("House with a great view", "San Francisco, CA 94110"," This is 2 bed /1 bath home boasts an enormous, \n", 2,1,840353, "pic1", true));
-
-        recyclerViewPopular = (RecyclerView) this.view.findViewById(R.id.viewPopular);
-        recyclerViewNew = (RecyclerView) this.view.findViewById(R.id.viewNews);
-
-        recyclerViewPopular.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        recyclerViewNew.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-
-        adapterNew = new ItemsAdapter(itemsArrayList);
-        adapterPopular = new ItemsAdapter(itemsArrayList);
-
-        recyclerViewPopular.setAdapter(adapterPopular);
-        recyclerViewNew.setAdapter(adapterNew);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void get(View ro){
+        Request request = new Request.Builder().url(getUrl).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("API FAILURE", "FAILILLLLLLLLLLLLLLLLLLLLLLLL");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //Log.d("API SUCCESSS", response.body().string());
+
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            String responseData = response.body().string();
+                            JSONObject json = new JSONObject(responseData);
+                            JSONArray array = new JSONArray(json.get("data").toString());
+
+                            ArrayList<SitesModel> sites = new ArrayList<>();
+
+                            for(int i=0; i < array.length() ; i++){
+                                sites.add(siteService.jsonToSitesModel(array.getJSONObject(i)));
+                            }
+
+                            listViewAll = ro.findViewById(R.id.listSites);
+                            adapterList = new SitesAdapter(sites, ro.getContext());
+                            listViewAll.setAdapter(adapterList);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
